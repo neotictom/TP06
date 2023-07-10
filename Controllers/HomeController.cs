9c1c22de1b87976f0f88102f785178bb083c146a
TOMAS NEIMERMAN GROSSO <47496739@ort>
2023-07-10 10:21:43 -0300

    5

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,58 +1,78 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TP06.Models;
 
-namespace TP06.Controllers;
-
-public class HomeController : Controller
+namespace TP06.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly BD _bd;
 
-    public IActionResult Index()
-    {  
-        int i = 0;
-        ViewBag.ListaCandidatos = BD.ListarCandidato(i);
-        ViewBag.ListaPartidos = BD.ListarPartido();
-        return View("Index");
-    }
-    public IActionResult VerDetallePartido(int idPartido){
-        
-        ViewBag.InfoPartido = BD.VerInfoPartido(idPartido);
-        return View("VerDetallePartido");
-    }
-    public IActionResult VerDetalleCandidato(int idCandidato){
-        
-        return View();
-    }
-    public IActionResult AgregarCandidato(int idPartido){
+        public HomeController(ILogger<HomeController> logger, BD bd)
+        {
+            _logger = logger;
+            _bd = bd;
+        }
 
-        return View();
-    }
-    [HttpPost] IActionResult GuardarCandidato(Candidato Can){
-        return View();
-    }
-    public IActionResult EliminarCandidato(int idCandidato, int idPartido){
-        return View();
-    }
-    public IActionResult Elecciones(){
-        return View();
-    }
-    public IActionResult Creditos(){
-        return View();
-    }
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {  
+            ViewBag.ListaPartidos = _bd.ListarPartido(); 
+            return View("Index");
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult VerDetallePartido(int idPartido)
+        {
+            ViewBag.InfoPartido = _bd.VerInfoPartido(idPartido);
+            ViewBag.ListaCandidatos = _bd.ListarCandidatos(idPartido); 
+            return View("VerDetallePartido");
+        }
+
+        public IActionResult VerDetalleCandidato(int idCandidato)
+        {
+            ViewBag.InfoCandidato = _bd.VerInfoCandidato(idCandidato); 
+            return View("VerDetalleCandidato");
+        }
+
+        public IActionResult AgregarCandidato(int idPartido)
+        {
+            ViewBag.IdPartido = idPartido; // Agregar el IdPartido al ViewBag
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GuardarCandidato(Candidato can)
+        {
+            _bd.GuardarCandidato(can); // Agregar el método GuardarCandidato() en la clase BD para guardar el candidato en la base de datos
+            return RedirectToAction("VerDetallePartido", new { idPartido = can.IdPartido }); 
+        }
+
+        public IActionResult EliminarCandidato(int idCandidato, int idPartido)
+        {
+            _bd.EliminarCandidato(idCandidato); // Agregar el método EliminarCandidato() en la clase BD para eliminar el candidato de la base de datos
+            return RedirectToAction("VerDetallePartido", new { idPartido }); 
+        }
+        public IActionResult Elecciones()
+        {
+            return View();
+        }
+
+        public IActionResult Creditos()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
+
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,54 +1,76 @@
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using Dapper;
-public class BD{
-    public static string _connectionString = @"Server=localhost; DataBase=Elecciones;Trusted_Connection=True;";
-    public static void AgregarCandidato(Candidato can){
-        string sql = "INSERT INTO Candidato (IdCandidato, IdPartido, Apellido, Nombre, Fechanacimiento, Foto, Postulacion) " + "VALUES (@IdCandidato, @IdPartido, @Apellido, @Nombre, @Fechanacimiento, @Foto, @Postulacion);";
-        using (SqlConnection conexion = new SqlConnection(_connectionString)){
-        conexion.Execute(sql, new {IdCandidato = can.idCandidato, IdPartido = can.idPartido, Apellido = can.Apellido, Nombre = can.Nombre, Fechanacimiento = can.FechaNacimiento, Foto = can.Foto, Postulacion = can.Postulacion});
+
+public class BD
+{
+    private static string _connectionString = @"Server=localhost; DataBase=Elecciones;Trusted_Connection=True;";
+
+    public static void AgregarCandidato(Candidato can)
+    {
+        string sql = "INSERT INTO Candidato (IdCandidato, IdPartido, Apellido, Nombre, Fechanacimiento, Foto, Postulacion) " +
+                     "VALUES (@IdCandidato, @IdPartido, @Apellido, @Nombre, @Fechanacimiento, @Foto, @Postulacion);";
+
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            conexion.Execute(sql, can);
         }
     }
-    public static void EliminarCandidato(int idCandidato){
+
+    public static void EliminarCandidato(int idCandidato)
+    {
         string consulta = "DELETE FROM Candidato WHERE IdCandidato = @IdCandidato;";
-    using (SqlConnection conexion = new SqlConnection(_connectionString))
+
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            conexion.Execute(consulta, new { IdCandidato = idCandidato });
+        }
+    }
+
+    public static Partido VerInfoPartido(int idPartido)
     {
-        conexion.Execute(consulta, new { IdCandidato = idCandidato });
+        Partido info = null;
+
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT * FROM Partido WHERE IdPartido = @IdPartido;";
+            info = conexion.QueryFirstOrDefault<Partido>(sql, new { IdPartido = idPartido });
+        }
+
+        return info;
     }
-    }
-    public static Partido VerInfoPartido(int idPartido){
-    Partido info = null;
-    using (SqlConnection conexion = new SqlConnection(_connectionString))
-    {
-        string sql = "SELECT * FROM Partido WHERE IdPartido = @IdPartido;";
-        info = conexion.QueryFirstOrDefault<Partido>(sql, new {IdPartido = idPartido});
-    }
-    return info;
-    }
+
     public static Candidato VerInfoCandidato(int idCandidato)
     {
-    Candidato info = null;
-    using (SqlConnection conexion = new SqlConnection(_connectionString))
-    {
-        string sql = "SELECT * FROM Candidato WHERE IdCandidato = @IdCandidato;";
-        info = conexion.QuerySingleOrDefault<Candidato>(sql, new { IdCandidato = idCandidato });
-    }
-    return info;
-    }
-    public static List<Partido> ListarPartido()
-    {
-    string sql = "SELECT IdPartido, Nombre, Logo, Sitioweb, Fechafundacion, Cantidaddiputados, Cantidadsenadores FROM Partido;";
+        Candidato info = null;
 
-    using (SqlConnection conexion = new SqlConnection(_connectionString))
-    {
-        return conexion.Query<Partido>(sql).ToList();
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT * FROM Candidato WHERE IdCandidato = @IdCandidato;";
+            info = conexion.QuerySingleOrDefault<Candidato>(sql, new { IdCandidato = idCandidato });
+        }
+
+        return info;
     }
-    }
-    public static List<Candidato> ListarCandidato(int idPartido)
+
+    public static List<Partido> ListarPartidos()
     {
-    string sql = "SELECT IdCandidato, IdPartido, Apellido, Nombre, Fechanacimiento, Foto, Postulacion FROM Candidato WHERE IdPartido = @IdPartido;";
-    using (SqlConnection conexion = new SqlConnection(_connectionString))
-    {
-        return conexion.Query<Candidato>(sql, new { IdPartido = idPartido }).ToList();
+        string sql = "SELECT IdPartido, Nombre, Logo, Sitioweb, Fechafundacion, Cantidaddiputados, Cantidadsenadores FROM Partido;";
+
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            return conexion.Query<Partido>(sql).ToList();
+        }
     }
+
+    public static List<Candidato> ListarCandidatos(int idPartido)
+    {
+        string sql = "SELECT IdCandidato, IdPartido, Apellido, Nombre, Fechanacimiento, Foto, Postulacion " +
+                     "FROM Candidato WHERE IdPartido = @IdPartido;";
+
+        using (SqlConnection conexion = new SqlConnection(_connectionString))
+        {
+            return conexion.Query<Candidato>(sql, new { IdPartido = idPartido }).ToList();
+        }
     }
 }
